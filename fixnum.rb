f67acd002cafb95a9b602c599bf2159e8653d465
ldv1642 <ldv1642>
2012-03-15 11:19:41 +0000

    updated

---
+++ fixnum.rb
@@ -0,0 +1,26 @@
+class Fixnum
+  def method_missing(method_id,*args)
+    name = method_id.to_s #convert meth_id from a symbol
+    if name =~ /^(second|minute|hour|day|year)$/ #regex for either of these 3
+      self.send(name+'s') #send to receiver, self with names + s
+    else
+      super #pass up to superclass original method call to look
+      #for a valid reciever
+    end
+    
+  end
+  def seconds; self; end
+  def minutes; seconds * 60; end
+  def hours; minutes * 60; end
+  def days; hours * 24; end
+  def years; days * 365; end
+  
+  def ago
+    Time.now - self
+  end
+  def to_go
+    Time.now + self
+  end
+end
+
+puts 23.years.to_go
--- movie.rb
+++ movie.rb
@@ -2,32 +2,40 @@ class Movie
   #class variable
   @@include_movie = false
   #class method
-  def Movie.include_year=(new_value)
+  def self.include_year=(new_value)
     @@include_movie = new_value
   end
-  
+
   def full_title
     if @@include_movie
+      #use self instead of @title to create another layer of
+      #encapsulation. allow us to change the getter/setter later if needed
       "#{self.title} #{self.year}"
     else
       self.title
     end
     
   end
-  
+  #default constructor
   def initialize(title,year)
     @title = title
     @year = year
   end
   
-  def title
-    @title
-  end
   
-  def title=(new_title)
+  def change_title(new_title)
     @title = new_title
   end
   
+  #use attr_accessor instead of manually making year & =year if 
+  #simple getter/setter is required.
+  #attr_reader = getter
+  #attr_writer = setter
+  #this is a form of metaprogramming, creating code at runtime
+  attr_accessor :title
+  attr_accessor :year
+  
+ /# 
   def year
     @year
   end
@@ -35,9 +43,10 @@ class Movie
   def year=(new_year)
     @year = new_year
   end
+  #/
   
 end
-
-m = Movie.new("Cool movie", "1987")
 Movie.include_year=true
+b = [23,4,6]
+m = Movie.new("Cool movie", "1987")
 puts m.full_title
