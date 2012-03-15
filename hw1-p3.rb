4a048e872a2cd09305f24b41f25f1f65bb51a1a3
ldv1642 <ldv1642>
2012-03-15 17:31:17 +0000

    fuckin loop

--- hw1-p3.rb
+++ hw1-p3.rb
@@ -1,43 +1,60 @@
 words = ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 
-'scream'] 
+'scream', ''] 
+
+store = []
+counter = 0
+found = 1
+
+words.select do |w|
+  w #only work with words, "" will be set for already matched words
+end.each do |d|
+  print "added #{d}\n"
+  store.insert(counter,[d])
+  print "looking for matches for #{d}\n"
+  words.each do |ww|#look for any matches
+    if !(store.include? ww)#check for duplicates
+      if d.chars.sort.join == ww.chars.sort.join#check for match, add if found
+        print "found match in #{ww}\n"
+        store[counter].insert(found,ww)
+        found += 1
+      else
+      end
+    else
+    end
+  end
+  counter += 1
+end
+
+print store
 
 def combine_anagrams(words)
   sortedGroups =[]
   counter = 0
+  found = 1
   
   words.each do |w|
-    sortedGroups.insert(counter,[w])
-    print "#{sortedGroups}\n" 
-    words.each do |ww|
-      print "comparing #{w} and #{ww}\n"
-      if w == ww;
-        words.delete(w)
-        
-      elsif w.chars.sort.join == ww.chars.sort.join
-        print "#{sortedGroups}\n"
-        sortedGroups[counter].insert(sortedGroups.length,ww)
-        words.delete(w)
-
+    if w != ""
+      sortedGroups.insert(counter,[w])
+      puts "added new word #{w}"
+      print "#{sortedGroups}\n" 
+      
+      words.each do |ww|
+        print "comparing #{w} and #{ww}\n"
+        if w.include? ww
+          
+        elsif w.chars.sort.join == ww.chars.sort.join
+          print "inserting #{ww}\n"
+          sortedGroups[counter].insert(found,ww)
+          found += 1
+          words.index w = ""
+        end
       end
-    end
-    counter +=1
-  end
-print sortedGroups
-/#
-  words.select do |w|
-     w
-  end.map do |w|
-    w = w.chars.sort.join
-    words.select do |ww|
-      if ww.chars.sort.join == w
-        puts w
-      end
+      puts "getting next word"
+      counter +=1
     end
   end
-  #/
-              
-  #print sorted
 end
+#combine_anagrams(words)
 
-combine_anagrams(words)
+
 
