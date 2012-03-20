4478f33e2c83ca78f6cd771956370be81973edcb
ldv1642 <ldv1642>
2012-03-20 14:24:59 +0000

    updated

--- hw1-p1a.rb
+++ hw1-p1a.rb
@@ -1,8 +1,21 @@
 def palindrome?(string)
   test = string.downcase
-  test = test.gsub(/\W/,'')
-  #print test == test.reverse
+  test = test.gsub(/\W/i,'')
   return test == test.reverse
 end
 
+def count_words(string)
+#store words along with count in h
+h = Hash.new()
+#count words in string
+string.downcase.gsub(/\w+/).each do |w|
+    if h.has_key?(w)
+      c = h[w]
+      h[w] = c += 1
+    else
+      h[w] = 1
+    end
+  end
+return h
+end
 
